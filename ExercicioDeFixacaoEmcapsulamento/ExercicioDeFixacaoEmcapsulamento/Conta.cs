5146dc43c2e8685ff358d2a50afdef8e21700418
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:53:53 -0300

    Update Conta.cs

--- ExercicioDeFixacaoEmcapsulamento/ExercicioDeFixacaoEmcapsulamento/Conta.cs
+++ ExercicioDeFixacaoEmcapsulamento/ExercicioDeFixacaoEmcapsulamento/Conta.cs
@@ -1,36 +1,42 @@
-﻿using System;
-using System.Reflection.Metadata.Ecma335;
+using System;
 
 namespace ExercicioDeFixacaoEmcapsulamento
 {
     class Conta
     {
-        public int Id { get; private set; }
-        public string Nome { get; set; }
-        public double Saldo { get; private set; }
+        public int Id { get; private set; }     // Propriedade para armazenar o ID da conta (somente leitura).
+        public string Nome { get; set; }        // Propriedade para armazenar o nome do titular da conta (leitura e escrita).
+        public double Saldo { get; private set; } // Propriedade para armazenar o saldo da conta (somente leitura).
 
+        // Construtor para inicializar a conta com ID e nome do titular.
         public Conta(int id, string nome)
         {
             Id = id;
             Nome = nome;
         }
 
-        public Conta(int id, string nome, double saldo) : this(id, nome){
+        // Construtor para inicializar a conta com ID, nome do titular e saldo.
+        public Conta(int id, string nome, double saldo) : this(id, nome)
+        {
             Saldo = saldo;
         }
 
-        public void Deposito(double quantia){
+        // Método para realizar um depósito na conta.
+        public void Deposito(double quantia)
+        {
             Saldo += quantia;
         }
 
+        // Método para realizar um saque na conta (incluindo taxa de $5).
         public void Saque(double saque)
         {
             Saldo -= (saque + 5);
         }
 
+        // Sobrescrita do método ToString() para retornar uma representação formatada das informações da conta.
         public override string ToString()
         {
-            return $"conta {Id}, Titular: {Nome}, Saldo: $ {Saldo.ToString("F2")}";
+            return $"Conta {Id}, Titular: {Nome}, Saldo: $ {Saldo.ToString("F2")}";
         }
     }
 }
