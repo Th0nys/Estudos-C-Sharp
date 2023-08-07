22210b59702e549a36de875443fbfe4a00edaf6b
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:57:23 -0300

    Update Calculadora.cs

--- MembrosEstaticosPt1/MembrosEstaticosPt1/Calculadora.cs
+++ MembrosEstaticosPt1/MembrosEstaticosPt1/Calculadora.cs
@@ -1,16 +1,18 @@
-﻿using System;
+using System;
 
 namespace MembrosEstaticosPt1
 {
     class Calculadora
     {
-        public static double Pi = 3.14;
+        public static double Pi = 3.14; // Declaração de um campo estático para armazenar o valor de Pi.
 
+        // Método estático para calcular a circunferência de uma esfera.
         public static double Circunferencia(double r)
         {
             return 2.0 * Pi * r;
         }
 
+        // Método estático para calcular o volume de uma esfera.
         public static double Volume(double r)
         {
             return 4.0 / 3 * Pi * Math.Pow(r, 3);
