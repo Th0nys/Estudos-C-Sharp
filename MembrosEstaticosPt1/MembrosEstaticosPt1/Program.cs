63fb0b2ba50eb62659b4801e0d5b8b7900fa885b
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:57:52 -0300

    Update Program.cs

--- MembrosEstaticosPt1/MembrosEstaticosPt1/Program.cs
+++ MembrosEstaticosPt1/MembrosEstaticosPt1/Program.cs
@@ -1,4 +1,4 @@
-﻿using MembrosEstaticosPt1;
+using MembrosEstaticosPt1; // Importa o namespace MembrosEstaticosPt1 para usar a classe Calculadora.
 using System;
 
 namespace Course
@@ -7,16 +7,15 @@ namespace Course
     {
         static void Main(string[] args)
         {
-
             Console.Write("Entre o valor do raio: ");
-            double raio = double.Parse(Console.ReadLine());
+            double raio = double.Parse(Console.ReadLine()); // Lê o valor do raio da esfera inserido pelo usuário.
 
-            double circ = Calculadora.Circunferencia(raio);
-            double volume = Calculadora.Volume(raio);
+            double circ = Calculadora.Circunferencia(raio); // Calcula a circunferência utilizando o método estático Circunferencia da classe Calculadora.
+            double volume = Calculadora.Volume(raio); // Calcula o volume utilizando o método estático Volume da classe Calculadora.
 
-            Console.WriteLine($"Circunferencia: {circ.ToString("F2")}");
-            Console.WriteLine($"Volume: {volume.ToString("F2")}");
-            Console.WriteLine($"Valor de PI: {Calculadora.Pi.ToString("F2")}");
+            Console.WriteLine($"Circunferencia: {circ.ToString("F2")}"); // Exibe o valor da circunferência formatado com duas casas decimais.
+            Console.WriteLine($"Volume: {volume.ToString("F2")}"); // Exibe o valor do volume formatado com duas casas decimais.
+            Console.WriteLine($"Valor de PI: {Calculadora.Pi.ToString("F2")}"); // Exibe o valor de Pi formatado com duas casas decimais.
         }
     }
 }
