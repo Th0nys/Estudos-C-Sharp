725a46392c373e8e73baec70a1049c8e0be007de
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:46:48 -0300

    Update Program.cs

--- EstruturaCondicional(While)/EstruturaCondicional(While)/Program.cs
+++ EstruturaCondicional(While)/EstruturaCondicional(While)/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Course
 {
@@ -7,16 +7,18 @@ namespace Course
         static void Main(string[] args)
         {
             Console.Write("Digite um numero: ");
-            double x = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine()); // Lê o número inserido pelo usuário.
 
+            // Loop que continua enquanto o número inserido for não negativo.
             while (x >= 0.0)
             {
-                double raiz = Math.Sqrt(x);
-                Console.WriteLine(raiz.ToString("F3"));
-                Console.Write("Digite outro numero: ");
-                x = double.Parse(Console.ReadLine());
+                double raiz = Math.Sqrt(x); // Calcula a raiz quadrada do número inserido.
+                Console.WriteLine(raiz.ToString("F3")); // Exibe a raiz quadrada com três casas decimais.
+                Console.Write("Digite outro numero: "); // Solicita ao usuário para inserir outro número.
+                x = double.Parse(Console.ReadLine()); // Lê o novo número inserido.
             }
-            Console.WriteLine("Numero Negativo!");
+
+            Console.WriteLine("Numero Negativo!"); // Exibe essa mensagem quando um número negativo for inserido.
         }
     }
 }
