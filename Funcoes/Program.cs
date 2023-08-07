d5bec562cf9c0f6441d127c8ef6fb343b13407c4
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:56:19 -0300

    Update Program.cs

--- Funcoes/Program.cs
+++ Funcoes/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel;
+using System;
 
 namespace Course
 {
@@ -7,30 +6,30 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
-            int n3 = int.Parse(Console.ReadLine());
+            int n1 = int.Parse(Console.ReadLine()); // Lê o primeiro número inteiro inserido pelo usuário.
+            int n2 = int.Parse(Console.ReadLine()); // Lê o segundo número inteiro inserido pelo usuário.
+            int n3 = int.Parse(Console.ReadLine()); // Lê o terceiro número inteiro inserido pelo usuário.
 
-            Console.WriteLine(Maior(n1, n2, n3));
+            Console.WriteLine(Maior(n1, n2, n3)); // Chama a função Maior e exibe o resultado.
         }
 
         static int Maior(int x, int y, int z)
         {
             int m;
 
-            if (x > y && x > z)
+            if (x > y && x > z) // Verifica se x é o maior número.
             {
                 m = x;
             }
-            else if (y > z && y > x) 
+            else if (y > z && y > x) // Verifica se y é o maior número.
             {
                 m = y;
             }
             else
             {
-                m = z;
+                m = z; // Se as duas condições anteriores forem falsas, z é o maior número.
             }
-            return m;
+            return m; // Retorna o maior número encontrado.
         }
     }
 }
