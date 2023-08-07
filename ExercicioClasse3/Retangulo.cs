ac2af8a9ecea9e506b6666e4ad0c1a6579f39480
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:50:05 -0300

    Update Retangulo.cs

--- ExercicioClasse3/Retangulo.cs
+++ ExercicioClasse3/Retangulo.cs
@@ -1,31 +1,31 @@
-﻿using System;
+using System;
 
 namespace ExercicioClasse3
 {
     class Retangulo
     {
-        public double Largura;
-        public double Altura;
+        public double Largura; // Campo público para armazenar a largura do retângulo.
+        public double Altura;  // Campo público para armazenar a altura do retângulo.
 
         public double Area()
         {
-            return Largura * Altura;
+            return Largura * Altura; // Calcula e retorna a área do retângulo.
         }
 
         public double Perimetro()
         {
-            return 2 * (Largura + Altura);
+            return 2 * (Largura + Altura); // Calcula e retorna o perímetro do retângulo.
         }
 
         public double Diagonal()
         {
-            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2)); // Calcula e retorna a diagonal do retângulo.
         }
 
         public override string ToString()
         {
+            // Retorna uma representação em string das informações do retângulo, incluindo área, perímetro e diagonal.
             return $"AREA = {Area().ToString("F2")}\nPERIMETRO: {Perimetro().ToString("F2")}\nDIAGONAL: {Diagonal().ToString("F2")}";
         }
     }
-
 }
