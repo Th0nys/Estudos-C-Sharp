665c1084eb8556c9b4ce203114e8c2a8533e72d8
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:54:32 -0300

    Update Program.cs

--- ExercicioDeFixacaoEmcapsulamento/ExercicioDeFixacaoEmcapsulamento/Program.cs
+++ ExercicioDeFixacaoEmcapsulamento/ExercicioDeFixacaoEmcapsulamento/Program.cs
@@ -1,4 +1,4 @@
-﻿using ExercicioDeFixacaoEmcapsulamento;
+using ExercicioDeFixacaoEmcapsulamento; // Importa o namespace ExercicioDeFixacaoEmcapsulamento para utilizar a classe Conta.
 using System;
 
 namespace Course
@@ -7,47 +7,48 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Conta conta;
+            Conta conta; // Declaração da variável para armazenar a instância de Conta.
 
             Console.Write("Entre o numero da conta: ");
-            int id = int.Parse(Console.ReadLine());
+            int id = int.Parse(Console.ReadLine()); // Lê o número da conta inserido pelo usuário.
 
             Console.Write("Entre o titular da conta: ");
-            string nome = Console.ReadLine();
+            string nome = Console.ReadLine(); // Lê o nome do titular inserido pelo usuário.
 
             Console.Write("Havera deposito inicial (s/n)? ");
-            char resp = char.Parse(Console.ReadLine());
+            char resp = char.Parse(Console.ReadLine()); // Lê a resposta do usuário sobre o depósito inicial.
+
             Console.WriteLine();
 
             if (resp == 's' || resp == 'S')
             {
                 Console.Write("Entre o valor do deposito inicial: ");
-                double depoInicial = double.Parse(Console.ReadLine());
-                conta = new Conta(id, nome, depoInicial);
+                double depoInicial = double.Parse(Console.ReadLine()); // Lê o valor do depósito inicial inserido pelo usuário.
+                conta = new Conta(id, nome, depoInicial); // Cria uma instância de Conta com depósito inicial.
             }
-            else{
-                conta = new Conta(id, nome);
+            else
+            {
+                conta = new Conta(id, nome); // Cria uma instância de Conta sem depósito inicial.
             }
 
             Console.WriteLine("Dados da conta:");
-            Console.WriteLine(conta);
+            Console.WriteLine(conta); // Chama o método 'ToString()' sobrescrito para exibir os dados da conta.
             Console.WriteLine();
 
             Console.Write("Entre o valor do deposito: ");
-            double depo = double.Parse(Console.ReadLine());
-            conta.Deposito(depo);
+            double depo = double.Parse(Console.ReadLine()); // Lê o valor do depósito inserido pelo usuário.
+            conta.Deposito(depo); // Realiza o depósito na conta.
 
             Console.WriteLine("Dados da conta atualizados: ");
-            Console.WriteLine(conta);
+            Console.WriteLine(conta); // Exibe os dados atualizados da conta.
             Console.WriteLine();
 
             Console.Write("Entre um valor para saque: ");
-            double saque = double.Parse(Console.ReadLine());
-            conta.Saque(saque);
+            double saque = double.Parse(Console.ReadLine()); // Lê o valor do saque inserido pelo usuário.
+            conta.Saque(saque); // Realiza o saque na conta.
 
             Console.WriteLine("Dados da conta atualizados: ");
-            Console.WriteLine(conta);
-
+            Console.WriteLine(conta); // Exibe os dados atualizados da conta.
         }
     }
 }
