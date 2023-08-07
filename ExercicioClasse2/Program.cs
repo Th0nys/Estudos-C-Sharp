cbdd43819393544f1b4b084c2bd5283e454dc99e
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:49:29 -0300

    Update Program.cs

--- ExercicioClasse2/Program.cs
+++ ExercicioClasse2/Program.cs
@@ -1,10 +1,5 @@
-﻿using ExercicioClasse2;
+using ExercicioClasse2; // Importa o namespace ExercicioClasse2 para utilizar a classe Funcionario.
 using System;
-
-/* EXERCÍCIO 02:
-Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
-médio dos funcionários.*/
-
 
 namespace Course
 {
@@ -12,24 +7,24 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Funcionario func1 = new Funcionario();
-            Funcionario func2 = new Funcionario();
+            Funcionario func1 = new Funcionario(); // Cria uma instância da classe Funcionario e atribui à variável func1.
+            Funcionario func2 = new Funcionario(); // Cria outra instância da classe Funcionario e atribui à variável func2.
 
             Console.WriteLine("Dados do primeiro funcionario: ");
             Console.Write("Nome: ");
-            func1.name = Console.ReadLine();
-            Console.Write("salario: ");
-            func1.wage = double.Parse(Console.ReadLine());
+            func1.name = Console.ReadLine(); // Atribui o nome do primeiro funcionário.
+            Console.Write("Salario: ");
+            func1.wage = double.Parse(Console.ReadLine()); // Atribui o salário do primeiro funcionário.
 
             Console.WriteLine("Dados do segundo funcionario: ");
             Console.Write("Nome: ");
-            func2.name = Console.ReadLine();
-            Console.Write("salario: ");
-            func2.wage = double.Parse(Console.ReadLine());
+            func2.name = Console.ReadLine(); // Atribui o nome do segundo funcionário.
+            Console.Write("Salario: ");
+            func2.wage = double.Parse(Console.ReadLine()); // Atribui o salário do segundo funcionário.
 
             double middle;
-            middle = (func1.wage + func2.wage) / 2;
-            Console.WriteLine($"Salario Medio = {middle.ToString("F2")}");
+            middle = (func1.wage + func2.wage) / 2; // Calcula o salário médio dos funcionários.
+            Console.WriteLine($"Salario Medio = {middle.ToString("F2")}"); // Exibe o salário médio formatado com duas casas decimais.
         }
     }
 }
