83d52ea79e9bc8ee1e4733f7309bca2c8e1fcc1d
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:51:03 -0300

    Update Funcionario.cs

--- ExercicioClasse4/ExercicioClasse4/Funcionario.cs
+++ ExercicioClasse4/ExercicioClasse4/Funcionario.cs
@@ -1,26 +1,27 @@
-﻿using System;
+using System;
 
 namespace ExercicioClasse4
 {
     class Funcionario
     {
-        public string Nome;
-        public double SalarioBruto;
-        public double Imposto;
+        public string Nome;          // Campo público para armazenar o nome do funcionário.
+        public double SalarioBruto;  // Campo público para armazenar o salário bruto do funcionário.
+        public double Imposto;       // Campo público para armazenar o valor do imposto sobre o salário.
 
         public double SalarioLiquido()
         {
-            return SalarioBruto - Imposto;
+            return SalarioBruto - Imposto; // Calcula e retorna o salário líquido do funcionário.
         }
 
         public void AumentarSalario(double porcentagem)
         {
-             porcentagem = (SalarioBruto * porcentagem) / 100;
-             SalarioBruto += porcentagem;
+            porcentagem = (SalarioBruto * porcentagem) / 100; // Calcula o aumento em dólares.
+            SalarioBruto += porcentagem; // Aumenta o salário bruto do funcionário.
         }
 
         public override string ToString()
         {
+            // Retorna uma representação em string das informações do funcionário, incluindo nome e salário líquido.
             return $"Funcionario: {Nome}, ${SalarioLiquido().ToString("F2")}";
         }
     }
