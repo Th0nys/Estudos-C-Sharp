e0c1ec42e23b97df9485b745dcf539ee3ac8d6f8
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:52:25 -0300

    Update Aluno.cs

--- ExercicioClasse5/Aluno.cs
+++ ExercicioClasse5/Aluno.cs
@@ -1,31 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace ExercicioClasse5
 {
     class Aluno
     {
-        public string Nome;
-        public double NotaA, NotaB, NotaC;
+        public string Nome;        // Campo público para armazenar o nome do aluno.
+        public double NotaA, NotaB, NotaC; // Campos públicos para armazenar as notas do aluno.
 
         public double SomaNotas()
         {
-            return NotaA + NotaB + NotaC;
+            return NotaA + NotaB + NotaC; // Calcula e retorna a soma das notas do aluno.
         }
 
         public override string ToString()
         {
             if (SomaNotas() >= 60)
             {
-                return $"Nota Final = {SomaNotas().ToString("F2")}\nAPROVADO";
+                return $"Nota Final = {SomaNotas().ToString("F2")}\nAPROVADO"; // Retorna uma mensagem de aprovação se a nota final for maior ou igual a 60.
             }
             else
             {
                 double faltaram = 60.0 - SomaNotas();
-                return $"Nota Final = {SomaNotas().ToString("F2")}\nREPROVADO\nFaltaram {faltaram.ToString("F2")} PONTOS";
+                return $"Nota Final = {SomaNotas().ToString("F2")}\nREPROVADO\nFaltaram {faltaram.ToString("F2")} PONTOS"; // Retorna uma mensagem de reprovação se a nota final for menor que 60, mostrando quantos pontos faltaram.
             }
         }
     }
