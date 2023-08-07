42e805a8ef9526e1812845528d9e627b1d5398af
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:50:31 -0300

    Update Program.cs

--- ExercicioClasse3/Program.cs
+++ ExercicioClasse3/Program.cs
@@ -1,4 +1,4 @@
-﻿using ExercicioClasse3;
+using ExercicioClasse3; // Importa o namespace ExercicioClasse3 para utilizar a classe Retangulo.
 using System;
 
 namespace Course
@@ -7,12 +7,12 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Retangulo retangulo = new Retangulo();
+            Retangulo retangulo = new Retangulo(); // Cria uma instância da classe Retangulo e atribui à variável 'retangulo'.
 
             Console.WriteLine("Entre a largura e altura de um retangulo: ");
-            retangulo.Largura = double.Parse(Console.ReadLine());
-            retangulo.Altura = double.Parse(Console.ReadLine());
-            Console.WriteLine($"{retangulo}");
+            retangulo.Largura = double.Parse(Console.ReadLine()); // Lê a largura do retângulo inserida pelo usuário.
+            retangulo.Altura = double.Parse(Console.ReadLine());  // Lê a altura do retângulo inserida pelo usuário.
+            Console.WriteLine($"{retangulo}"); // Chama o método 'ToString()' sobrescrito para exibir as informações formatadas do retângulo.
         }
     }
 }
