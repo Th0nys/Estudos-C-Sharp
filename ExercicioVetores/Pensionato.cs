2ab5037535b6439aa328f1920db7ddc96ff04f9a
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:55:05 -0300

    Update Pensionato.cs

--- ExercicioVetores/Pensionato.cs
+++ ExercicioVetores/Pensionato.cs
@@ -1,11 +1,10 @@
-﻿
 namespace ExercicioVetores
 {
     class Pensionato
     {
-        public string Name { get; set; }
-        public string Email { get; set;}
-        public int Room { get; set;}
+        public string Name { get; set; }  // Propriedade para armazenar o nome do estudante (leitura e escrita).
+        public string Email { get; set; } // Propriedade para armazenar o e-mail do estudante (leitura e escrita).
+        public int Room { get; set; }     // Propriedade para armazenar o número do quarto do estudante (leitura e escrita).
 
     }
 }
