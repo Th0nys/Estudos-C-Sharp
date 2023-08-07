fe17ffa01dcc59822894b08831e4beafeeecfa0e
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:53:08 -0300

    Update Program.cs

--- ExercicioClasse5/Program.cs
+++ ExercicioClasse5/Program.cs
@@ -1,4 +1,4 @@
-﻿using ExercicioClasse5;
+using ExercicioClasse5; // Importa o namespace ExercicioClasse5 para utilizar a classe Aluno.
 using System;
 
 namespace Course
@@ -7,14 +7,15 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Aluno aluno = new Aluno();
+            Aluno aluno = new Aluno(); // Cria uma instância da classe Aluno e atribui à variável 'aluno'.
+
             Console.Write("Nome do aluno: ");
-            aluno.Nome = Console.ReadLine();
-            Console.WriteLine("Digite as tres notas do aluno: ");
-            aluno.NotaA = double.Parse(Console.ReadLine());
-            aluno.NotaB = double.Parse(Console.ReadLine());
-            aluno.NotaC = double.Parse(Console.ReadLine());
-            Console.WriteLine($"{aluno}");
+            aluno.Nome = Console.ReadLine(); // Lê o nome do aluno inserido pelo usuário.
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.NotaA = double.Parse(Console.ReadLine()); // Lê a primeira nota inserida pelo usuário.
+            aluno.NotaB = double.Parse(Console.ReadLine()); // Lê a segunda nota inserida pelo usuário.
+            aluno.NotaC = double.Parse(Console.ReadLine()); // Lê a terceira nota inserida pelo usuário.
+            Console.WriteLine($"{aluno}"); // Chama o método 'ToString()' sobrescrito para exibir as informações do aluno.
         }
     }
 }
