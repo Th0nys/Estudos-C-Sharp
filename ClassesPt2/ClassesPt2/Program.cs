05773ac97f64d6d7026ee88cc02311417f403a8c
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:44:56 -0300

    Update Program.cs

--- ClassesPt2/ClassesPt2/Program.cs
+++ ClassesPt2/ClassesPt2/Program.cs
@@ -1,4 +1,4 @@
-﻿using ClassesPt2;
+using ClassesPt2; // Importa o namespace ClassesPt2 para utilizar a classe Produto.
 using System;
 
 namespace Course
@@ -7,15 +7,13 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            Produto p = new Produto("Tv", 900.00, 10); // Cria uma instância de Produto com nome "Tv", preço 900.00 e quantidade 10.
 
-            Produto p = new Produto("Tv", 900.00, 10);
+            p.Nome = "T"; // Define o nome do produto para "T" (aplica validação no setter da propriedade Nome).
 
-            p.Nome = "T";
-
-            Console.WriteLine(p.Nome);
-            Console.WriteLine(p.Preco.ToString("F2"));
-            Console.WriteLine(p.Quantidade);
-            
+            Console.WriteLine(p.Nome); // Imprime o nome do produto ("T").
+            Console.WriteLine(p.Preco.ToString("F2")); // Imprime o preço do produto formatado com duas casas decimais (900.00).
+            Console.WriteLine(p.Quantidade); // Imprime a quantidade em estoque do produto (10).
         }
     }
 }
