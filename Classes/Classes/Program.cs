2172b07d27650a9735839f88646bdab69c32b16c
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-07 07:42:58 -0300

    Update Program.cs

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -1,5 +1,5 @@
-﻿using Classes;
-using System;
+﻿using Classes; // Importa o namespace "Classes", onde está definida a classe Triangulo.
+using System; // Importa o namespace "System" para utilizar funcionalidades básicas.
 
 namespace Course
 {
@@ -7,24 +7,27 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Triangulo x, y;
-            x = new Triangulo();
-            y = new Triangulo();
+            Triangulo x, y; // Declara duas variáveis do tipo Triangulo chamadas x e y.
+            x = new Triangulo(); // Cria uma instância da classe Triangulo e atribui à variável x.
+            y = new Triangulo(); // Cria outra instância da classe Triangulo e atribui à variável y.
 
+            // Solicita ao usuário que insira as medidas do triângulo X.
             Console.WriteLine("Entre com as medidas do triangulo X: ");
             x.A = double.Parse(Console.ReadLine());
             x.B = double.Parse(Console.ReadLine());
             x.C = double.Parse(Console.ReadLine());
 
+            // Solicita ao usuário que insira as medidas do triângulo Y.
             Console.WriteLine("Entre com as medidas do triangulo Y: ");
             y.A = double.Parse(Console.ReadLine());
             y.B = double.Parse(Console.ReadLine());
             y.C = double.Parse(Console.ReadLine());
 
+            // Calcula a área dos triângulos x e y.
             double areaX = x.Area();
-
             double areaY = y.Area();
 
+            // Exibe as áreas calculadas com três casas decimais.
             Console.WriteLine($"Area de x: {areaX.ToString("F3")}");
             Console.WriteLine($"Area de y: {areaY.ToString("F3")}");
         }
