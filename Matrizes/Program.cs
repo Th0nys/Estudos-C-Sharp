6df3ef65ea0593ecb70a4e70cfbf7a8e5ba42105
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-08 07:25:33 -0300

    Update Program.cs

--- Matrizes/Program.cs
+++ Matrizes/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Course
 {
@@ -6,14 +6,39 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            double[,] mat = new double[2 ,2];
 
-            Console.WriteLine(mat.Length);
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(mat.Rank);
+            int[,] mat = new int[n, n];
 
-            Console.WriteLine(mat.GetLength(0));
-            Console.WriteLine(mat.GetLength(1));
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Main diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Negative numbers: " + count);
 
         }
     }
